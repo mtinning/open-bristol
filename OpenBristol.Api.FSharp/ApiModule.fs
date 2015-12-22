0b6f395cd4fd51099b6411163a6031d8a9b39b9e
mtinning <m.t.tinning@gmail.com>
2015-12-22 11:24:05 +0000

    Adding FSharp api project.

---
+++ OpenBristol.Api.FSharp/ApiModule.fs
@@ -0,0 +1,14 @@
+﻿namespace OpenBristol.Api.FSharp
+
+open System
+open System.Collections.Generic
+
+open FSharp.Interop.Dynamic
+
+open Nancy
+
+type ApiModule() =
+    inherit NancyModule("/api")
+
+    do
+        base.Get.["/{message}"] <- fun (p:Object) -> p?message :> Object
